b02ed6b9e8691deb6a9b9e4237860ce655a3f91f
Nandini <nandini@localhost.localdomain>
2021-10-28 23:56:00 +0530

    mera pahla pahla repository

---
+++ for_loop/table_using_for_loop.py
@@ -0,0 +1,29 @@
+for i in range(1,11,1):
+    print("1x"+str(i)+"="+str(i*1))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("2x"+str(i)+"="+str(i*2))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("3x"+str(i)+"="+str(i*3))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("4x"+str(i)+"="+str(i*4))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("5x"+str(i)+"="+str(i*5))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("6x"+str(i)+"="+str(i*6))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("7x"+str(i)+"="+str(i*7))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("8x"+str(i)+"="+str(i*8))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("9x"+str(i)+"="+str(i*9))
+print("\n==================\n")
+for i in range(1,11,1):
+    print("10x"+str(i)+"="+str(i*10))
