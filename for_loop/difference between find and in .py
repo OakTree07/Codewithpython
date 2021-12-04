ea39a1eecc8b6925305e928b140a4a0d9551eb01
OakTree07 <nandini.2003cs@gmail.com>
2021-12-04 13:06:13 +0530

    absolute and round

--- for_loop/difference between find and in .py
+++ for_loop/difference between find and in .py
@@ -1,2 +1,8 @@
-name="finding the darkness"
-print('fuck u' in name)
+#in always return the result in boolean type whereas find returns the output using indexing.
+name="finding the light"
+print('light' in name)
+
+
+
+name_1="Python for beginners"
+print(name_1.find("for"))
---
+++ for_loop/round and absolute function .py
@@ -0,0 +1,7 @@
+x=-1120
+print(x.__abs__())
+
+
+
+p=12.45
+print(x.__round__())
